fdc2c79b3472527c4fe969e905e1c5d52f6c6b56
Sofia.Ferragut <sofia.ferragut@gmail.com>
2019-05-12 20:58:41 -0300

    metodo agregado para ver que onda con los branches

--- app/src/main/java/ort/edu/tpindividual/MainActivity.java
+++ app/src/main/java/ort/edu/tpindividual/MainActivity.java
@@ -11,7 +11,5 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
     }
-    public String testMethod(){
-        return "helloWorld";
-    }
+
 }
